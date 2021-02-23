88a1ec2130c8a75c2f4e37b2c45316ada982e70f
czsquizer <56487372+czsquizer@users.noreply.github.com>
2021-02-23 11:24:56 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Squizer'
-description 'Simple whitelist resource edit for Amay'
+author 'Isigar'
+description 'Simple object whitelist resource edit of https://fivem-dev.cz/index.php?/topic/432-protekce-proti-spawnov%C3%A1n%C3%AD-objekt%C5%AF-a-vozidel-na-server-side/'
 version '1.0.0'
 
 server_script 'main.lua'
